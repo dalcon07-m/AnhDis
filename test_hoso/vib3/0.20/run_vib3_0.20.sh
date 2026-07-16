e3042edd65ded6710038ee141f7359250261e668
dalcon07-m <72202609+dalcon07-m@users.noreply.github.com>
2026-07-16 15:19:48 +0200

    Add files via upload

---
+++ test_hoso/vib3/0.20/run_vib3_0.20.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#PBS -u macias
+#PBS -N vib3_0.20
+#PBS -l nodes=qcexnod57:ppn=2
+#PBS -S /bin/bash
+FILE="vib3_0.20"
+export ScrDir=/scr/macias/${PBS_JOBID}_$FILE
+mkdir -p $ScrDir
+Wdir="/home/macias/bin/anhdis/anhdis2review/vib3/0.20"
+. /soft/g16.a03/g16/bsd/g16.profile
+cd $ScrDir
+g16 < $Wdir/$FILE.com > $ScrDir/$FILE.log
+cp *.log $Wdir
+cp *.o $Wdir
+cp *.e $Wdir
+exit
---
+++ test_hoso/vib4/0.00/run_vib4_0.00.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#PBS -u macias
+#PBS -N vib4_0.00
+#PBS -l nodes=qcexnod57:ppn=2
+#PBS -S /bin/bash
+FILE="vib4_0.00"
+export ScrDir=/scr/macias/${PBS_JOBID}_$FILE
+mkdir -p $ScrDir
+Wdir="/home/macias/bin/anhdis/anhdis2review/vib4/0.00"
+. /soft/g16.a03/g16/bsd/g16.profile
+cd $ScrDir
+g16 < $Wdir/$FILE.com > $ScrDir/$FILE.log
+cp *.log $Wdir
+cp *.o $Wdir
+cp *.e $Wdir
+exit
